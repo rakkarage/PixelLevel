d918a5b82778b907f6cb0b4d2306e6c63f248419
Rakka Rage <rakkarage@gmail.com>
2020-06-02 19:47:35 -0400

    x

--- PixelLevel/Level.gd
+++ PixelLevel/Level.gd
@@ -1,48 +1,35 @@
 extends Viewport
 
+const _zoomMin := Vector2.ONE
+const _zoomMinMin := Vector2(0.8, 0.8)
+const _zoomMax := Vector2(32.0, 32.0)
+const _zoomMaxMax := Vector2(40.0, 40.0)
 onready var _camera := $Camera
 onready var _back   := $Back
 onready var _fore   := $Fore
 onready var _mob    := $Mob
 onready var _target := $Target
 onready var _nav    := $Nav
+onready var _tween  := $Tween
 onready var _size: Vector2 = _back.cell_size
-
 var _path := PoolVector2Array()
 var _drag := false
-var _t : Transform2D
-# var _scale := 2.0
-
-# func _ready() -> void:
-	# _t = get_canvas_transform()
-	# _t = _t.scaled(Vector2(_scale, _scale))
-	# set_canvas_transform(_t)
-	# _setLimit()
-
-# func _setLimit() -> void:
-# 	var rect = _back.get_used_rect();
-# 	_camera.limit_top = 0
-# 	_camera.limit_left = 0
-# 	_camera.limit_right = rect.end.x * _back.cell_size.x
-# 	_camera.limit_bottom = rect.end.y * _back.cell_size.y
 
 func _tilePos(tile: Vector2) -> Vector2:
 	return _back.map_to_world(_back.world_to_map(tile))
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
-		if event.pressed:
-			_target.global_position = _tilePos(event.global_position)
-			_drag = true
-		else:
-			_drag = false
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			if event.pressed:
+				_target.global_position = _tilePos(event.global_position)
+				_drag = true
+			else:
+				_drag = false
+		elif event.button_index == BUTTON_WHEEL_UP:
+			_camera.zoom -= Vector2(0.2, 0.2)
+		elif event.button_index == BUTTON_WHEEL_DOWN: 
+			_camera.zoom += Vector2(0.2, 0.2)
 	elif event is InputEventMouseMotion:
 		if _drag:
-			_camera.position -= event.relative
-			# print(event.relative)
-			# _t = _t.translated(event.relative)
-			# _t = _t.scaled(Vector2(_scale, _scale))
-			# print(_t[2])
-			# set_canvas_transform(_t)
-
-# get_simple_path from mob to target!!!
+			_camera.global_position -= event.relative
