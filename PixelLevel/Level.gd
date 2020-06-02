5d6ced9ccdc89bd2a5a6de347a6c21c59956a0fc
Rakka Rage <rakkarage@gmail.com>
2020-06-02 09:25:54 -0400

    cam

--- PixelLevel/Level.gd
+++ PixelLevel/Level.gd
@@ -1,15 +1,32 @@
 extends Node2D
-class_name Level
 
-onready var back := $Back
-onready var fore := $Back
-onready var mob := $Mob
-onready var target := $Target
-onready var nav := $Nav
+onready var _back := $Back
+onready var _fore := $Back
+onready var _mob := $Mob
+onready var _target := $Target
+onready var _nav := $Nav
+var _path := PoolVector2Array()
+var _drag := false
+var _t : Transform2D
+
+
+func _ready() -> void:
+	_t = get_viewport().get_canvas_transform()
+	_t = _t.scaled(Vector2(2,2))
+	get_viewport().set_canvas_transform(_t)
 
 func _input(event: InputEvent) -> void:
-	if not event is InputEventMouseButton:
-		return
-	if event.button_index != BUTTON_LEFT or not event.pressed:
-		return
-	print(back.world_to_map(event.position))
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			if event.pressed:
+				var map = _back.world_to_map(event.position)
+				_target.position = _back.map_to_world(map)
+				_drag = true
+			else:
+				_drag = false
+	elif event is InputEventMouseMotion:
+		if _drag:
+			_t[2] = -_mob.position
+			get_viewport().set_canvas_transform(_t)
+
+# get_simple_path from mob to target!!!
