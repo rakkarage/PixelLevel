01c0e3aaed315270acff6d19f52cb981ceab82d7
Rakka Rage <rakkarage@gmail.com>
2020-06-02 09:38:04 -0400

    x

--- PixelLevel/Level.gd
+++ PixelLevel/Level.gd
@@ -1,18 +1,20 @@
-extends Node2D
+extends Node
 
-onready var _back := $Back
-onready var _fore := $Back
-onready var _mob := $Mob
-onready var _target := $Target
-onready var _nav := $Nav
+onready var _back   := $Container/Viewport/Back
+onready var _fore   := $Container/Viewport/Fore
+onready var _mob    := $Container/Viewport/Mob
+onready var _target := $Container/Viewport/Target
+onready var _nav    := $Container/Viewport/Nav
 var _path := PoolVector2Array()
 var _drag := false
 var _t : Transform2D
+var _scale := 2.0
 
+# get_viewport_rect().size
 
 func _ready() -> void:
 	_t = get_viewport().get_canvas_transform()
-	_t = _t.scaled(Vector2(2,2))
+	_t = _t.scaled(Vector2(_scale, _scale))
 	get_viewport().set_canvas_transform(_t)
 
 func _input(event: InputEvent) -> void:
