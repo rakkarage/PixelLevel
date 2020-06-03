8373fa81fb653e226b0060d427b02aedb561c563
Rakka Rage <rakkarage@gmail.com>
2020-06-02 23:41:51 -0400

    x

--- PixelLevel/Level.gd
+++ PixelLevel/Level.gd
@@ -1,11 +1,11 @@
 extends Viewport
 
-onready var _camera := $Camera
-onready var _back   := $Back
-onready var _fore   := $Fore
-onready var _mob    := $Mob
-onready var _target := $Target
-onready var _astar := AStar2D.new()
+onready var _camera       := $Camera
+onready var _back: TileMap = $Back
+onready var _fore: TileMap = $Fore
+onready var _mob          := $Mob
+onready var _target       := $Target
+onready var _astar        := AStar2D.new()
 var _path := PoolVector2Array()
 var _drag := false
 
@@ -16,6 +16,7 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT:
 			if event.pressed:
+				# if target already here???
 				_target.global_position = _tilePos(event.global_position * _camera.zoom + _camera.global_position)
 				_drag = true
 			else:
@@ -29,4 +30,14 @@ func _input(event: InputEvent) -> void:
 			_camera.global_position -= event.relative * _camera.zoom
 
 func _findPath() -> void:
+	# from mob to target and draw it with effect
 	pass
+
+func _ready() -> void:
+	_addPoints();
+
+func _addPoints() -> void:
+	var size = _back.get_used_rect().size
+	for y in range(size.y):
+		for x in range(size.x):
+			_astar.add_point(x + (y * size.x), Vector2(x, y))
