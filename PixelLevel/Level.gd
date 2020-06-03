ece4cac9156c2f093571c437dda65c0794d93f42
Rakka Rage <rakkarage@gmail.com>
2020-06-03 12:11:40 -0400

    x

--- PixelLevel/Level.gd
+++ PixelLevel/Level.gd
@@ -1,10 +1,12 @@
 extends Viewport
 
+const _duration = 0.333
 onready var _camera       := $Camera
 onready var _back: TileMap = $Back
 onready var _fore: TileMap = $Fore
 onready var _mob          := $Mob
 onready var _target       := $Target
+onready var _tween        := $Tween
 onready var _astar        := AStar2D.new()
 var _path := PoolVector2Array()
 var _drag := false
@@ -12,35 +14,41 @@ var _rect := Rect2()
 
 func _ready() -> void:
 	_rect = _back.get_used_rect()
-	print("rect: %s" % _rect)
 	_addPoints();
+	# hide target when on mob and set target to mob pos and hide @ start@
 
 func _index(p: Vector2) -> int:
 	return int(p.x + (p.y * _rect.size.x))
 
+func _point(i: int) -> Vector2:
+	var y = i / _rect.size.x
+	var x = i - _rect.size.x * y
+	return Vector2(x, y);
+
 func _addPoints() -> void:
-	for y in range(size.y):
-		for x in range(size.x):
+	for y in range(_rect.size.y):
+		for x in range(_rect.size.x):
 			var p = Vector2(x, y)
 			_astar.add_point(_index(p), p)
 
-func _tilePosition(tile: Vector2) -> Vector2:
+func _world(tile: Vector2) -> Vector2:
 	return _back.map_to_world(tile)
 
-func _tileAt(position: Vector2) -> Vector2:
+func _map(position: Vector2) -> Vector2:
 	return _back.world_to_map(position)
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT:
+			# use button up? else it is a drag!!!!!!!!!!!
 			if event.pressed:
-				# if target already here???
-				var tile = _tileAt(event.global_position * _camera.zoom + _camera.global_position)
-				_target.global_position = _tilePosition(tile)
-				if _rect.has_point(tile):
-					print("in")
-				else:
-					print("out")
+				var map = _map(event.global_position * _camera.zoom + _camera.global_position)
+				var target = _map(_target.global_position)
+				if map.is_equal_approx(target):
+					print("double")
+				_target.global_position = _world(map)
+				if not _rect.has_point(map):
+					_targetToClosest(map)
 				_drag = true
 			else:
 				_drag = false
@@ -51,6 +59,14 @@ func _input(event: InputEvent) -> void:
 	elif event is InputEventMouseMotion:
 		if _drag:
 			_camera.global_position -= event.relative * _camera.zoom
+
+func _targetToClosest(to: Vector2) -> void:
+	_targetTo(_astar.get_point_position(_astar.get_closest_point(to)))
+
+func _targetTo(to: Vector2) -> void:
+	_tween.stop(_target, "position")
+	_tween.interpolate_property(_target, "position", null, _world(to), _duration, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
+	_tween.start()
 
 func _findPath() -> void:
 	# from mob to target and draw it with effect
