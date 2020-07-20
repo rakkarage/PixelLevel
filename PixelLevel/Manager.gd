cad4ad7e922d87da64d28e81433bf08d537c123c
Rakka Rage <rakkarage@gmail.com>
2020-07-20 18:16:28 -0400

    x

--- PixelLevel/Manager.gd
+++ PixelLevel/Manager.gd
@@ -47,12 +47,12 @@ func _updateMap() -> void:
 	_imageTexture.create_from_image(_image)
 
 onready var _g := {
-	GenerateBasic.new(_level): 100,
-	GenerateRoom.new(_level): 100,
-	GenerateDungeon.new(_level): 33,
-	GenerateMaze.new(_level): 33,
-	GenerateCave.new(_level): 10,
-	GenerateWalker.new(_level): 10,
+	GenerateBasic.new(_level): 7,
+	GenerateRoom.new(_level): 6,
+	GenerateDungeon.new(_level): 5,
+	GenerateMaze.new(_level): 4,
+	GenerateCave.new(_level): 3,
+	GenerateWalker.new(_level): 2,
 	GenerateTemplate.new(_level): 1,
 }
 
