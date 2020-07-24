180c8e89a80a94475f2d1cb8492d4edb969e5937
Rakka Rage <rakkarage@gmail.com>
2020-07-24 15:52:54 -0400

    x

---
+++ PixelMob/TestRandom.gd
@@ -0,0 +1,13 @@
+extends Control
+
+onready var _random := $Button
+onready var _mob := $Mob
+
+func _ready() -> void:
+	Utility.ok(_random.connect("pressed", self, "_randomPressed"))
+
+func _randomPressed() -> void:
+	match Random.next(3):
+		0: _mob.attack()
+		1: _mob.walk()
+		2: _mob.flip_h = not _mob.flip_h
