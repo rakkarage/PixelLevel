5d0f1643351ea083be7bafa4066ec2a2b41cc51d
Rakka Rage <rakkarage@gmail.com>
2023-05-12 03:02:11 -0400

    x

--- PixelLevel/Utility.gd
+++ PixelLevel/Utility.gd
@@ -3,39 +3,35 @@ class_name Utility
 
 static func stfu(_ignore) -> void: pass
 
-static func indexV(position: Vector2, width: int) -> int:
-	return index(int(position.x), int(position.y), width)
+static func indexV(p: Vector2, width: int) -> int:
+	return index(int(p.x), int(p.y), width)
 
 static func index(x: int, y: int, width: int) -> int:
 	return int(y * width + x)
 
-static func position(index: int, width: int) -> Vector2:
-	var y := int(index / float(width))
-	var x := int(index - width * y)
+static func position(i: int, width: int) -> Vector2:
+	var y := int(i / float(width))
+	var x := int(i - width * y)
 	return Vector2(x, y)
 
-static func range(value: int, count: int) -> Array:
+static func arraySequence(value: int, count: int) -> Array:
 	var array := []
 	for i in range(count):
 		array.append(value + i)
 	return array
 
-static func repeat(value, count: int) -> Array:
+static func arrayRepeat(value, count: int) -> Array:
 	var array := []
 	for _i in range(count):
 		array.append(value)
 	return array
 
-static func listFiles(path: String) -> Array:
+func _listFiles(path: String) -> Array:
 	var list := []
-	var dir := DirAccess.open(path)
-	if dir && dir.list_dir_begin() == OK: # TODOGODOT4 fill missing arguments https://github.com/godotengine/godot/pull/40547
-		var file := dir.get_next()
-		while file != "":
-			var newPath := path + "/" + file
-			if dir.current_is_dir():
-				list += listFiles(newPath)
-			elif !file.begins_with(".") and !file.ends_with(".godot"):
-				list.append(newPath)
-			file = dir.get_next()
+	for dir in DirAccess.get_directories_at(path):
+		if !dir.begins_with("."):
+			list += _listFiles(path + "/" + dir)
+	for file in DirAccess.get_files_at(path):
+		if !file.begins_with(".") and !file.ends_with(".import"):
+			list.append(path + "/" + file)
 	return list
